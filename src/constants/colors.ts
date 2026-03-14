aa045f487c45861561d1267b75618fd8abc39064
seojoonrp <seojoonrp@snu.ac.kr>
2026-03-14 19:33:08 +0900

    feat: Set router folder structure

---
+++ src/constants/colors.ts
@@ -0,0 +1,21 @@
+export const Colors = {
+  black: {
+    dark: "#111112",
+    mid: "#17171A",
+    light: "#29282D",
+  },
+  white: "#FAFAFA",
+  point: {
+    coral: "#E53153",
+    strong: "#D12041",
+  },
+  text: {
+    light: "#9999A0",
+    mid: "#5D5D64",
+    dark: "#484850",
+  },
+  disabled: "#7F7F84",
+  brand: {
+    kakao: "#FEE500",
+  },
+} as const;
